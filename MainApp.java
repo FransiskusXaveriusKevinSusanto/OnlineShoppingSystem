00778a7203e842d9f97cf9d7f632fa18967bb24c
FransiskusXaveriusKevinSusanto <kevinsusantofx@gmail.com>
2025-05-09 10:37:52 +0700

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -1,1 +1,12 @@
+public class MainApp { 
+    public static void main(String[] args) {
+        Electronics laptop = new Electronics("ASUS Vivobook", 1999.99, 1001, "ASUS", 24); 
+        Clothing hoodie = new Clothing("Hoodie Polos", 10.25, 2002, "L"); 
 
+        System.out.println("=== Produk Elektronik ===");
+        System.out.println(laptop.getInfo());
+
+        System.out.println("\n=== Produk Pakaian ===");
+        System.out.println(hoodie.getInfo());
+    }
+}
