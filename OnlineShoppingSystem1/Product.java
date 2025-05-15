292b6d4a59cec8c5f55b7f6e4e04ced20be314c0
FransiskusXaveriusKevinSusanto <kevinsusantofx@gmail.com>
2025-05-15 09:02:50 +0700

    Add files via upload

---
+++ OnlineShoppingSystem1/Product.java
@@ -0,0 +1,42 @@
+public class Product {
+    private String name;
+    private double price;
+    private int productId;
+
+    // Constructor
+    public Product(String name, double price, int productId) {
+        this.name = name;
+        this.price = price;
+        this.productId = productId;
+    }
+
+    // Getter dan Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    // Method getInfo
+    public String getInfo() {
+        return name + " - $" + price;
+    }
+}
