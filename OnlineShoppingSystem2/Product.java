31d8987316e75c322b9aef657cc631d45f79e716
FransiskusXaveriusKevinSusanto <kevinsusantofx@gmail.com>
2025-05-17 21:51:56 +0700

    Update Product.java

--- OnlineShoppingSystem2/Product.java
+++ OnlineShoppingSystem2/Product.java
@@ -1,1 +1,17 @@
+import java.time.LocalDateTime;
 
+public abstract class Product {
+    protected LocalDateTime createdAt;
+
+    public Product() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public abstract String getCategory();
+
+    public abstract String getInfo();
+}
