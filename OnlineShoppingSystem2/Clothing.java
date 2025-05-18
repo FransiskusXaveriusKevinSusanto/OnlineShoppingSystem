1a1b01de36a96710ebb4bf889cd39d7ddeb1a153
Abyaz <92667195+Cro1ssant07@users.noreply.github.com>
2025-05-18 13:22:06 +0700

    Update Clothing.java

--- OnlineShoppingSystem2/Clothing.java
+++ OnlineShoppingSystem2/Clothing.java
@@ -1,1 +1,27 @@
+public class Clothing extends Product implements Discountable {
+    private String name;
+    private double price;
+    private Size size;
 
+    public Clothing(String name, double price, Size size) {
+        super();
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+
+    @Override
+    public String getCategory() {
+        return "Clothing";
+    }
+
+    @Override
+    public String getInfo() {
+        return "Name: " + name + ", Size: " + size + ", Price: " + price;
+    }
+
+    @Override
+    public double getDiscountedPrice(double discountPercentage) {
+        return price * (1 - discountPercentage / 100);
+    }
+}
